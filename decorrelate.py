f99b83a3ed6b461d05afda31eef160accb0135ae
Lorenz Gaertner <lorenz.gaertner@physik.uni-muenchen.de>
2024-03-14 19:16:13 +0100

    take care of already correlated parameters

--- decorrelate.py
+++ decorrelate.py
@@ -28,21 +28,23 @@ def validate(corr):
             raise ValueError("Number of variables does not match dimension of correlation matrix")
         
 def get_coords(var_name, spec):
+    coords = []
     for ich, ch in enumerate(spec):
         for isa, sa in enumerate(ch["samples"]):
             for imo, mo in enumerate(sa["modifiers"]):
                 if mo["name"] == var_name:
-                    return ich, isa, imo
+                    coords.append((ich, isa, imo))
+    return coords
     
-def group_coords(coords):
+def group_coords(coords, icoords):
     # group coords by channel and sample, but remember index
     group_coords = {}
-    for i, c in enumerate(coords):
+    for c, i in zip(coords, icoords):
         if (c[0], c[1]) not in group_coords.keys():
-            group_coords[(c[0], c[1])] = []
-        group_coords[(c[0], c[1])].append((i, c[2]))
+            group_coords[(c[0], c[1])] = {"uv_ind": [], "mod_ind": []}
+        group_coords[(c[0], c[1])]["uv_ind" ].append(i)
+        group_coords[(c[0], c[1])]["mod_ind"].append(c[2])
     return group_coords
-
 
 def decorrelate(spec):
     if "correlations" in spec.keys():
@@ -50,7 +52,7 @@ def decorrelate(spec):
         
         validate(spec["correlations"])
         
-        spec = deepcopy(spec)
+        channels = deepcopy(spec["channels"])
         
         for corr in spec["correlations"]:
             
@@ -59,13 +61,16 @@ def decorrelate(spec):
             
             # get channel, sample and modifier index for each variable
             coords = []
-            for var in corr["vars"]:
-                coords.append(get_coords(var, spec["channels"]))
+            icoords = []
+            for ic, var in enumerate(corr["vars"]):
+                c = get_coords(var, channels)
+                coords += get_coords(var, channels)
+                icoords += [ic for _ in c]
                 
-            coords_grouped = group_coords(coords)
+            coords_grouped = group_coords(coords, icoords)
             
-            mod_type  = [spec["channels"][c[0]]["samples"][c[1]]["modifiers"][c[2]]["type"] for c in coords]
-            mod_type  = np.unique(mod_type)
+            mod_type = [channels[c[0]]["samples"][c[1]]["modifiers"][c[2]]["type"] for c in coords]
+            mod_type = np.unique(mod_type)
             
             #check if all modifiers have the same type
             if len(mod_type) != 1:
@@ -74,22 +79,22 @@ def decorrelate(spec):
             
             # compute shifts for each independent eigenvector
             for i_uv, uv in enumerate(uvec.T):
-                for (ich, isa), mods_indexed in coords_grouped.items():
-                    mods = [m for _, m in mods_indexed]
-                    inds = [i for i, _ in mods_indexed]
+                for (ich, isa), mods in coords_grouped.items():
+                    uv_ind  = mods["uv_ind"]
+                    mod_ind = mods["mod_ind"]
                     
-                    nom = np.array(spec["channels"][ich]["samples"][isa]["data"])
+                    nom = np.array(channels[ich]["samples"][isa]["data"])
                     
-                    lo_diffs = np.array([np.subtract(spec["channels"][ich]["samples"][isa]["modifiers"][mo]["data"]["lo_data"], nom) for mo in mods])
-                    hi_diffs = np.array([np.subtract(spec["channels"][ich]["samples"][isa]["modifiers"][mo]["data"]["hi_data"], nom) for mo in mods])
+                    lo_diffs = np.array([np.subtract(channels[ich]["samples"][isa]["modifiers"][imo]["data"]["lo_data"], nom) for imo in mod_ind])
+                    hi_diffs = np.array([np.subtract(channels[ich]["samples"][isa]["modifiers"][imo]["data"]["hi_data"], nom) for imo in mod_ind])
                     
-                    lo_shift = np.sum(uv[inds, np.newaxis] * lo_diffs, axis=0)
-                    hi_shift = np.sum(uv[inds, np.newaxis] * hi_diffs, axis=0)
+                    lo_shift = np.sum(uv[uv_ind, np.newaxis] * lo_diffs, axis=0)
+                    hi_shift = np.sum(uv[uv_ind, np.newaxis] * hi_diffs, axis=0)
 
                     new_lo = nom + lo_shift
                     new_hi = nom + hi_shift
                     
-                    spec["channels"][ich]["samples"][isa]["modifiers"].append(
+                    channels[ich]["samples"][isa]["modifiers"].append(
                         {
                             "name": corr["name"] + f"[{str(i_uv)}]",
                             "type": mod_type,
@@ -100,14 +105,11 @@ def decorrelate(spec):
                         }
                     )
                     
-            for ch_sa, mods in coords_grouped.items():
-                ich, isa = ch_sa
+            for (ich, isa), _ in coords_grouped.items():
                 new_modifiers = []
-                for m in spec["channels"][ich]["samples"][isa]["modifiers"]:
+                for m in channels[ich]["samples"][isa]["modifiers"]:
                     if m["name"] not in corr["vars"]:
                         new_modifiers.append(m)
-                spec["channels"][ich]["samples"][isa]["modifiers"] = new_modifiers
+                channels[ich]["samples"][isa]["modifiers"] = new_modifiers
         
-        del spec["correlations"]
-        
-    return spec
+    return {"channels": channels}
